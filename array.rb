cfaffc2b994024fc7efb76feaabb959adf2b4408
Dayana <dayanalon83@gmail.com>
2018-11-09 15:29:29 +0000

    add array

---
+++ array.rb
@@ -0,0 +1,53 @@
+## define array
+
+# animals = ['antelope', 'bear', 'dog', 'cat']
+# puts animals.inspect
+
+# puts '--------'
+
+# animals =%w(antilope, bear, dog, cat)
+# puts animals.inspect
+
+# puts '--------'
+
+# animals =%w(antilope, bear, dog, cat)
+# puts animals [0]
+# puts animals [2]
+
+# puts '--------'
+
+# ## push into array
+# animals = %w(antelop, bear, dog, cat)
+# animals << 'crabs'
+# puts animals.inspect
+
+## puts '--------'
+
+## select array 
+# animals = %w(antelop, bear, dog, cat, crabs)
+# puts animals.select {|a| a.size == 5}
+
+## puts '--------'
+
+
+# # ## nested array
+# numbers = [[1,2,4], [5,6,7,]]
+# puts numbers.size
+# puts numbers.flatten.inspect
+
+
+## puts '--------'
+
+## include?
+# animals = %w(antelop bear dog cat crabs)
+# puts animals.include? 'dog'
+# puts animals.include? "dogs"
+
+## puts '--------'
+
+##empty?
+
+# animals =[]
+# puts animals.empty?
+
+
